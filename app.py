11acdfb7c53a623bce0a1251dd003a8e1c990abb
vincent93113 <30742232+vincent93113@users.noreply.github.com>
2023-12-04 14:45:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+from flask_ngrok import run_with_ngrok
 
 import tempfile, os
 import datetime
@@ -16,6 +17,7 @@ from fn import *
 
 from dotenv import load_dotenv
 load_dotenv()
+dicts = []
 
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
@@ -23,6 +25,7 @@ handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 # handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 app = Flask(__name__)
+
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 @app.route("/callback", methods=['POST'])
@@ -35,40 +38,99 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
+dicts = [0]
 @handler.add(PostbackEvent)
 def handle_message(event):
+    global dicts
     recieved_msg = event.postback.data
     if 'med_kr' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'med_kr'
+        
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)    
     elif 'school_kr' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'school_kr'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
     elif 'love_kr' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'love_kr'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
     elif 'law_kr' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'law_kr'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
     elif 'comedy_kr' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'comedy_kr'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
     elif 'med_us' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'med_us'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
     elif 'school_us' in recieved_msg:
-        message = TextSendMessage(text='嗨')
-        line_bot_api.reply_message(event.reply_token, message)
+        key = 'school_us'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
+        line_bot_api.reply_message(event.reply_token,message)
+        return
     elif 'love_us' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'love_us'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
     elif 'comedy_us' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'comedy_us'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
     elif 'law_us' in recieved_msg:
-        message = TextSendMessage(text='嗨')
+        key = 'law_us'
+        dicts = get_dicts(key)
+        message = FlexSendMessage(alt_text='影集推薦',contents=flex_func(key))
         line_bot_api.reply_message(event.reply_token, message)
+    elif 'getplot' in recieved_msg:
+        if recieved_msg == 'getplot0':
+            message = TextSendMessage(text=dicts[0]['name'] + '的劇情介紹為:' + '\n'+dicts[0]['introduce'])
+            line_bot_api.reply_message(event.reply_token, message)
+            # print(dicts)
+        elif recieved_msg == 'getplot1':
+            message = TextSendMessage(text=dicts[1]['name'] + '的劇情介紹為:' + '\n'+dicts[1]['introduce'])
+            line_bot_api.reply_message(event.reply_token, message)
+        elif recieved_msg == 'getplot2':
+            message = TextSendMessage(text=dicts[2]['name'] + '的劇情介紹為:' + '\n'+dicts[2]['introduce'])
+            line_bot_api.reply_message(event.reply_token, message)
+        elif recieved_msg == 'getplot3':
+            message = TextSendMessage(text=dicts[3]['name'] + '的劇情介紹為:' + '\n'+dicts[3]['introduce'])
+            line_bot_api.reply_message(event.reply_token, message)
+        elif recieved_msg == 'getplot4':
+            message = TextSendMessage(text=dicts[4]['name'] + '的劇情介紹為:' + '\n'+dicts[4]['introduce'])
+            line_bot_api.reply_message(event.reply_token, message)
+    elif 'getactor' in recieved_msg:
+
+        if recieved_msg == 'getactor0':
+            message = TextSendMessage(text=dicts[0]['name'] + '的演員為:\n'+dicts[0]['actor'] +'和'+dicts[0]['actress'])
+            line_bot_api.reply_message(event.reply_token, message)
+        elif recieved_msg == 'getactor1':
+            message = TextSendMessage(text=dicts[1]['name'] + '的演員為:\n' +dicts[1]['actor'] +'和'+dicts[1]['actress'])
+            line_bot_api.reply_message(event.reply_token, message)
+        elif recieved_msg == 'getactor2':
+            message = TextSendMessage(text=dicts[2]['name'] + '的演員為:\n' +dicts[2]['actor'] +'和'+dicts[2]['actress'])
+            line_bot_api.reply_message(event.reply_token, message)
+        elif recieved_msg == 'getactor3':
+            message = TextSendMessage(text=dicts[3]['name'] + '的演員為:\n' +dicts[3]['actor'] +'和'+dicts[3]['actress'])
+            line_bot_api.reply_message(event.reply_token, message)
+        elif recieved_msg == 'getactor4':
+            message = TextSendMessage(text=dicts[4]['name'] + '的演員為:\n' +dicts[4]['actor'] +'和'+dicts[4]['actress'])
+            line_bot_api.reply_message(event.reply_token, message)
+
+
 
 @handler.add(MessageEvent)
 def handle_message(event):
@@ -77,16 +139,30 @@ def handle_message(event):
     if event_type == 'text':
         recieved_msg = event.message.text
         if '韓劇' in recieved_msg:
-            message = TextSendMessage(text='那你想看哪種韓劇呢?',quick_reply=QuickReply(items_korea))
+            country = 'kr'
+            # print(item_func(country))
+            message = TextSendMessage(text='那你想看哪種韓劇呢?',quick_reply=item_func(country))
             line_bot_api.reply_message(event.reply_token,message)
         elif '美劇' in recieved_msg:
-            message = TextSendMessage(text='那你想看哪種美劇呢?',quick_reply=QuickReply(items_us))
+            country = 'us'
+            message = TextSendMessage(text='那你想看哪種美劇呢?',quick_reply=item_func(country))
             line_bot_api.reply_message(event.reply_token,message)
-        
+        elif '肚子餓' in recieved_msg:
+            message = TextSendMessage(text='推薦給你兩款泡麵 ')
+            message1 = image_carousel_message()
+            message = [message,message1]
+            line_bot_api.reply_message(event.reply_token,message)
+        elif '嗨' in recieved_msg:
+            message = TextSendMessage(text='嗨 要我推薦你影集的話可以按下方按鈕或輸入 韓劇 or 美劇 ')
+
+            line_bot_api.reply_message(event.reply_token,message)
+        # elif 'test' in recieved_msg:
+        #     message = FlexSendMessage(alt_text='hello',contents=flex_content)
+        #     line_bot_api.reply_message(event.reply_token,message)
     elif event_type == 'image':
         message = TextSendMessage(text='這是圖片')
         line_bot_api.reply_message(event.reply_token, message)
-
+ 
     
     
     
